d7ac3033c5d292e9a46178b87df9be5a6510c05c
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-11-19 15:08:41 -0700

    added final score

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
   let isGoingRight = false
   let leftTimeId
   let rightTimeId
+  let score = 0
 
   function createDoodler(){
     grid.appendChild(doodler)
@@ -53,6 +54,16 @@ document.addEventListener('DOMContentLoaded', ()=>{
         platform.bottom -= 4
         let visual = platform.visual
         visual.style.bottom = platform.bottom + 'px'
+
+        if (platform.bottom < 10){
+          let firstPlatform = platforms[0].visual
+          firstPlatform.classList.remove('platform')
+          platforms.shift()
+          score ++
+          console.log(platforms)
+          let newPlatform = new Platform(600)
+          platforms.push(newPlatform)
+        }
       })
     }
   }
@@ -94,8 +105,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
   function gameOver(){
     console.log('game over')
     isGameOver = true
+    while (grid.firstChild){
+      grid.removeChild(grid.firstChild)
+    }
+    grid.innerHTML = score
     clearInterval(upTimerId)
     clearInterval(downTimerId)
+    clearInterval(leftTimeId)
+    clearInterval(rightTimeId)
   }
 
   function control(){
@@ -119,7 +136,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       doodlerLeftSpace -=5
       doodler.style.left = doodlerLeftSpace + 'px'
     } else moveRight()
-    },30)
+    },20)
   }
 
   function moveRight(){
@@ -133,7 +150,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       doodlerLeftSpace +=5
       doodler.style.left = doodlerLeftSpace + 'px'
     } else moveLeft()
-    },30)
+    },20)
   }
 
   function moveStraight(){
